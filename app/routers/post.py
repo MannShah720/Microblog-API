6ae6fbda6966fa6e7f08627dd3b89bb2e2674caf
Mann Shah <shahmann0108@gmail.com>
2026-09-23 21:50:59 +0100

    only owner can delete and update post functionality

--- app/routers/post.py
+++ app/routers/post.py
@@ -38,13 +38,18 @@ def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends
 # ========= Delete a post =========
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id == id)
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
 
-    if post.first() == None:
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"Post with id {id} not found")
-    post.delete(synchronize_session=False)
 
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
+                            detail=f"Not authorised to perform requested action")
+    
+    post_query.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
@@ -58,6 +63,10 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"Post with id {id} not found")
 
+    if post.owner_id != current_user.id:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
+                                detail=f"Not authorised to perform requested action")
+    
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
     return post_query.first()
