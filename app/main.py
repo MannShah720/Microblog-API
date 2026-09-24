8f4639ca648ecd3ddda01785a23400ea3c090a66
Mann Shah <shahmann0108@gmail.com>
2026-09-24 23:03:37 +0100

    set up cors middleware

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from . database import engine
 from .routers import post, user, auth, vote
@@ -16,6 +17,16 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
