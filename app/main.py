c85c6be05e2f645f1289b0beb8193f8580327284
Mann Shah <shahmann0108@gmail.com>
2026-09-24 22:44:56 +0100

    alembic db setup complete

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
